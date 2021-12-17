5349cea4c8f2befc3b0ec8522e08512616a63822
Jianmin Chen <jianmin0chen@gmail.com>
2021-12-17 05:57:08 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask_mail import Mail
+from flask_session import Session
+from os import environ
+
+from database import db
+from routes import general
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    """Add and edit headers so responses aren't cached."""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+# Configure email
+app.config["MAIL_USERNAME"] = environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = environ.get("MAIL_PASSWORD")
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
+mail = Mail(app)
+
+# Configure session
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure routes
+app.register_blueprint(general.blueprint)
---
+++ routes/general.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, current_app, flash, redirect, render_template, request, session
+from flask_mail import Mail, Message
+from sqlalchemy import select
+
+# Configure blueprint
+blueprint = Blueprint("general", __name__)
+
+
+@blueprint.route("/")
+def index():
+    """Return main page or dashboard depending on whether user is logged in or not."""
+    return render_template("index.html")
+
+
+@blueprint.route("/signup", methods=["GET", "POST"])
+def signup():
+    """Route users use to sign up for an account."""
+    if request.method == "GET":
+        return render_template("signup.html")
+
+    return redirect("/")
